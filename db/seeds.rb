27343338199e96b931df68e9f5dfc7d9a8632059
Julio <juliodevasquez@gmail.com>
2017-10-14 15:55:13 -0500

    added more dummy data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,30 @@
 	# require 'faker'
 
  
+#Customers
 @customer_1 =	Customer.create!(name:Faker::Name.name, last_name: Faker::Name.last_name, zipcode: Faker::Address.zip_code)
 
+@customer_2 =	Customer.create!(name:Faker::Name.name, last_name: Faker::Name.last_name, zipcode: Faker::Address.zip_code)
 
+@customer_3 =	Customer.create!(name:Faker::Name.name, last_name: Faker::Name.last_name, zipcode: Faker::Address.zip_code)
+
+@customer_4 =	Customer.create!(name:Faker::Name.name, last_name: Faker::Name.last_name, zipcode: Faker::Address.zip_code)
+
+@customer_5 =	Customer.create!(name:Faker::Name.name, last_name: Faker::Name.last_name, zipcode: Faker::Address.zip_code)
+
+
+
+
+
+#Wines
 
 
 wine_1 = Wine.create!(name:Faker::Name.name, temp: Faker::Number.between(40,50), customer_id: @customer_1.id)
+
+wine_1 = Wine.create!(name:Faker::Name.name, temp: Faker::Number.between(40,50), customer_id: @customer_2.id)
+
+wine_1 = Wine.create!(name:Faker::Name.name, temp: Faker::Number.between(40,50), customer_id: @customer_3.id)
+
+wine_1 = Wine.create!(name:Faker::Name.name, temp: Faker::Number.between(40,50), customer_id: @customer_4.id)
+
+wine_1 = Wine.create!(name:Faker::Name.name, temp: Faker::Number.between(40,50), customer_id: @customer_5.id)
