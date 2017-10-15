152bf4b2ee431f769f8b6422030a449a1a9c19c6
Julio <juliodevasquez@gmail.com>
2017-10-14 20:22:33 -0500

    connected to api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+require 'net/http'
+require 'json'
+
+	def index
+
+		api_key = AUTH_DETAILS['api_key']
+		url = "http://api.openweathermap.org/data/2.5/weather?q=94040,US&APPID=#{api_key}"
+		uri = URI(url)
+		response = Net::HTTP.get(uri)
+		parsed =	JSON.parse(response)
+	end
+
 end
+
+
